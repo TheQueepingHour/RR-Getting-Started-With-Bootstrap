395df06f3c01ef8cab9e40e91e9d5e94dc804502
Josh Rivera <beps@Mactop.local>
2024-02-29 18:37:19 -0600

    Tested styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
 
 
 function App() {
   return (
     <div className="App">
+      <Nav activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href='/home'>Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='/about'>About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
       <h1>Bootstrap Website</h1>
-     
+      <Button variant='info'>Info</Button>
+      <Form>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' />
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Form.Group controlId='formBasicCheckbox'>
+          <Form.Check type='checkbox' label='Check me out' />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Form Dropdown with favorite movies</Form.Label>
+          <Form.Control as='select'>
+            <option>Jurassic Park</option>
+            <option>The Babysitter</option>
+            <option>Pulp Fiction</option>
+            <option>Tropic Thunder</option>
+            <option>Spiderman: No Way Home</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
